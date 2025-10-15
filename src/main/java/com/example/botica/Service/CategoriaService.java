862dec748acb2478b387660cf3af9053f4638206
Valery Gamboa <valerygamboaochoa@gmail.com>
2025-10-15 17:36:02 -0500

    Logica de guardar las categorias

---
+++ src/main/java/com/example/botica/Service/CategoriaService.java
@@ -0,0 +1,28 @@
+package com.example.botica.Service;
+
+import com.example.botica.Model.Categoria;
+import com.example.botica.Repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public String guardarCategoria(Long categoria_id, String tipo_categoria, String descripcion_categoria) {
+
+        Categoria categoria_nuevo = new Categoria();
+
+        categoria_nuevo.setId(categoria_id);
+        categoria_nuevo.setTipo_categoria(tipo_categoria);
+        categoria_nuevo.setDescripcion_categoria(descripcion_categoria);
+        categoriaRepository.save(categoria_nuevo);
+        return "Categoría guardada correctamente.";
+    }
+
+
+}

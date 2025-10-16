6e8bcda5b64392d34c57a83d629496a111b46b07
Anthon Joss <jossue.ramirezanthony@gmail.com>
2025-10-15 23:54:12 -0500

    *

--- src/main/java/com/example/botica/Controller/ProductoController.java
+++ src/main/java/com/example/botica/Controller/ProductoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
-@CrossOrigin(origins = { "http://127.0.0.1:5500", "http://localhost:5500" })
+@CrossOrigin(origins = "*")
 public class ProductoController {
 
     @Autowired

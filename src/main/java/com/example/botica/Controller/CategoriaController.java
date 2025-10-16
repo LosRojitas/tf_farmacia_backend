2a0ad16c912679886f54d4386985274285c66b80
Anthon Joss <jossue.ramirezanthony@gmail.com>
2025-10-16 11:50:18 -0500

    agregado * en categoria controller para front

--- src/main/java/com/example/botica/Controller/CategoriaController.java
+++ src/main/java/com/example/botica/Controller/CategoriaController.java
@@ -5,15 +5,13 @@ import com.example.botica.Model.Categoria;
 import com.example.botica.Repository.CategoriaRepository;
 import com.example.botica.Service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin(origins = "*")
 public class CategoriaController {
 
     @Autowired
